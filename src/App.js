758202422e4231c7988206b0930a28ae3f10b5b9
Nesh00 <c.nenad00@gmail.com>
2022-02-08 21:36:22 +0000

    create Header component and add default route to Articles

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
-import { Route, Routes } from 'react-router-dom';
+import { Navigate, Route, Routes } from 'react-router-dom';
 import Articles from './components/Articles/Articles';
 import Topics from './components/Topics/Topics';
 import ArticleById from './components/ArticleById/ArticleById';
 import './App.css';
+import Header from './components/Header/Header';
 
 const App = () => {
   return (
     <div className='App'>
+      <Header />
       <Topics />
       <Routes>
         <Route path={`/articles`} element={<Articles />} />
         <Route path={`/articles/:article_id`} element={<ArticleById />} />
+        <Route path={'*'} element={<Navigate to={'/articles'} replace />} />
       </Routes>
     </div>
   );
