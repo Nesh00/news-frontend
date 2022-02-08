8bfa7590a71205cd46a2da0932f9e84d01aad004
Nesh00 <c.nenad00@gmail.com>
2022-02-08 12:58:18 +0000

    add query links for each topic

--- src/components/Nav-Topics/Nav-Topics.jsx
+++ src/components/Nav-Topics/Nav-Topics.jsx
@@ -1,7 +1,7 @@
-import styles from './Nav-Topics.module.css';
 import { useEffect, useState } from 'react';
-import { getTopics } from '../../utils/api';
 import { Link } from 'react-router-dom';
+import { getTopics } from '../../utils/api';
+import styles from './Nav-Topics.module.css';
 
 const Topics = () => {
   const [topics, setTopics] = useState([]);
@@ -12,10 +12,20 @@ const Topics = () => {
 
   return (
     <ul className={styles.topics__container}>
+      <li key={'home'} className={styles.topics__item}>
+        <Link to={`/articles`} className={styles.topics__item}>
+          All Articles
+        </Link>
+      </li>
       {topics.map((topic) => {
         return (
-          <li key={topic.slug} className={styles.topics__item}>
-            <Link to={`/articles`}>{topic.slug}</Link>
+          <li key={topic.slug}>
+            <Link
+              to={`/articles?topic=${topic.slug}`}
+              className={styles.topics__item}
+            >
+              {topic.slug}
+            </Link>
           </li>
         );
       })}
